6aaefc0c93533fd98dbc4b8c34500203a5185359
hybridgreen <y.yayaoye@gmail.com>
2025-05-22 13:02:05 +0900

    Char counter did not count EVERYTHING. Updated to the correct solution

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# Imports
+from stats import *
+
+#Functions defs
 def get_book_text(book_path):
     with open(book_path) as book:
        return book.read()
-
+    
+#Defining main
 def main():
     try:
         #book_text = get_book_text(input("Please enter the books file path:"))
@@ -9,7 +14,11 @@ def main():
         #print(book_text)
         num_words = word_count(book_text)
         print(f"{num_words} words found in the document")
+        print (get_char_count(book_text))
+
+
     except Exception as e:
         print(e)
 
+#Running
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
+from string import ascii_lowercase
+
 def word_count(book):
     word_count = len(book.split())
     return word_count
+
+def get_char_count(book):
+    book = book.lower()
+    char_count = {}
+    for char in book:
+        if char in char_count:
+            char_count[char] +=1
+        else:
+            char_count[char] = 1
+    return char_count
